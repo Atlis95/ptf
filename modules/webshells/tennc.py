a8f1f919ac3547ca809aaa7b0f93faf244da565f
stanvdv <stan@ip4sure.nl>
2019-02-21 13:04:47 +0100

    to kali

--- modules/webshells/tennc.py
+++ modules/webshells/tennc.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
+#####################################
+# Installation module for WSO webshell from a backup source.
+#####################################
 
 # AUTHOR OF MODULE NAME
-AUTHOR="Stan vdv"
+AUTHOR="Jose Suarez (Josexv1)"
 
 # DESCRIPTION OF THE MODULE
-DESCRIPTION="This module will install/update all sort of webshells by tennc"
+DESCRIPTION="This module will install/update WSO - PHP webshell by Hardlinux"
 
 # INSTALL TYPE GIT, SVN, FILE DOWNLOAD
 # OPTIONS = GIT, SVN, FILE
 INSTALL_TYPE="GIT"
 
 # LOCATION OF THE FILE OR GIT/SVN REPOSITORY
-REPOSITORY_LOCATION="https://github.com/tennc/webshell"
+REPOSITORY_LOCATION="https://github.com/Josexv1/wso-webshell/"
 
 # WHERE DO YOU WANT TO INSTALL IT
-INSTALL_LOCATION="tennc-webshell"
+INSTALL_LOCATION="wso"
 
 # DEPENDS FOR DEBIAN INSTALLS
 DEBIAN="git"
